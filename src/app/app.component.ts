9bddc43dfcf38c3f117555d8e0ca313ce33ab26d
Pridon Tetradze <pridontetradze@gmail.com>
2023-06-30 13:05:43 +0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Editor } from '@tiptap/core';
+import StarterKit from '@tiptap/starter-kit';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
-  title = 'ng-tiptap';
+export class AppComponent implements OnDestroy {
+  editor = new Editor({
+    extensions: [StarterKit],
+  });
+
+  value =
+    '<blockquote>I think where I am not, therefore I am where I do not think.</blockquote>';
+
+  ngOnDestroy(): void {
+    this.editor.destroy();
+  }
 }
