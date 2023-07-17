18614f623c8e19f35d4f7030645507b19dec5da2
Lashvardi <lashakakashvili114@gmail.com>
2023-07-17 15:14:17 +0400

    Push

--- src/app/image-modal/image-modal.component.ts
+++ src/app/image-modal/image-modal.component.ts
@@ -10,27 +10,43 @@ import { ImageCroppedEvent } from 'ngx-image-cropper';
 })
 export class ImageModalComponent {
   imageUrl!: string;
-  aspectRatio = 1; // You can set a default value.
-  previewWidth = 200; // You can set a default value.
+  aspectRatio = 1;
+  previewWidth = 200;
+  resizeSettings = { width: this.previewWidth };
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+  showCropper = true; // Added this line
+
   constructor(private modalRef: NzModalRef, private sanitizer: DomSanitizer) {}
 
   submitForm(): void {
     this.modalRef.close(this.croppedImage);
   }
-  imageChangedEvent: any = '';
-  croppedImage: any = '';
 
   fileChangeEvent(event: any): void {
     this.imageChangedEvent = event;
   }
+
   imageCropped(event: ImageCroppedEvent) {
     const fileReader = new FileReader();
     fileReader.onload = (e) => {
-      console.log(fileReader.result); // This will print the base64 string to the console
+      console.log(fileReader.result);
       this.croppedImage = fileReader.result as string;
     };
     if (event.blob) {
       fileReader.readAsDataURL(event.blob);
     }
+  }
+
+  updateCropper(width: number) {
+    this.previewWidth = width;
+    this.resizeSettings = { width: this.previewWidth };
+    this.forceCropperRerender(); // Added this line
+  }
+
+  // Added this function
+  forceCropperRerender() {
+    this.showCropper = false;
+    setTimeout(() => this.showCropper = true, 0);
   }
 }
